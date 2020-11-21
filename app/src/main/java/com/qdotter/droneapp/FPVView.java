4a07c9d2ac222e6c1adf8d985b99a9e2b229fcb7
Oliver <otlg123@gmail.com>
2020-11-21 11:15:23 +0000

    WIP start adding FPV view

--- app/src/main/java/com/qdotter/droneapp/FPVView.java
+++ app/src/main/java/com/qdotter/droneapp/FPVView.java
@@ -1,4 +1,34 @@
 package com.qdotter.droneapp;
 
-public class FPVView {
+import android.app.Activity;
+import android.media.MediaFormat;
+import android.os.Bundle;
+
+import java.nio.ByteBuffer;
+
+import dji.sdk.camera.VideoFeeder;
+import dji.sdk.codec.DJICodecManager;
+
+public class FPVView extends Activity implements DJICodecManager.YuvDataCallback {
+
+    private static final String TAG = MainActivity.class.getName();
+    protected VideoFeeder.VideoDataListener mReceivedVideoDataListener = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        initUi();
+    }
+
+    void initUi()
+    {
+
+    }
+
+    @Override
+    public void onYuvDataReceived(MediaFormat mediaFormat, ByteBuffer byteBuffer, int dataSize, int width, int height) {
+
+    }
 }
